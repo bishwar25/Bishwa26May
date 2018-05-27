e18f219039242322d702fa13b45d905641230ee4
bishwar25 <bishwar25@gmail.com>
2018-05-27 16:36:28 +0530

    FirstJS26May_1

---
+++ firstJS.js
@@ -0,0 +1,35 @@
+// function getmethod(param){
+// console.log("Hello "+param)
+// }
+
+// getmethod("Bishwa")
+
+//let is basically used for looping for ex:
+
+// var variable=123
+// for(let abc=1;abc<100;abc++){
+//     //console.log(abc)
+// }
+// variable="abc"
+// abc=123
+
+//console.log(abc)
+
+
+// function method(){
+//     var var1=123;
+//     console.log(var1)
+// }
+// //console.log(var1)
+// console.log(variable)
+
+
+// console.log(variable)
+// var variable="Any String"
+console.log(method1)
+
+var method1=function(){
+    console.log("Hello")
+}
+//console.log(method1)
+method1()
